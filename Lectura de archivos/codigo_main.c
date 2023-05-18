2cf277983d5e688362c044bb7fd7088d1fb814fe
adrianvalgil <adrian.valgil@alumnos.upm.es>
2023-05-18 19:32:32 +0200

    Update codigo_main.c

--- Lectura de archivos/codigo_main.c
+++ Lectura de archivos/codigo_main.c
@@ -2,6 +2,18 @@
 #include "lectura.h"
 #include "lectura_2.h"
 
+
+
+//float mx() Calculo de la energia maxima de cada tecnologia
+//float mn() Calculo de la energia minima de cada tecnologia
+//float pro()  Calculo de la media de cada tecnologia
+//float rel()  Calculo del valor relativo de cada tecnologia respecto al total
+//void f_estadistica()  Fichero con el maximo, minimo y promedio de cada tecnologia
+//void f_nuevo()  En el caso de querer usar un fichero adjunto nuevo
+
+
+
+
 int main(){
 
     //Se deben introducir los parámetros de:
